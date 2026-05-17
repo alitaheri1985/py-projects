dd87e9726e3f5add55fb3f7c73f5b5d60b09c84e
Ali Taheri <ali_taheri1985@hotmail.com>
2026-05-17 17:08:17 +0330

    test.py renamed to project-5.py

---
+++ project-5.py
@@ -0,0 +1,43 @@
+import time
+
+password = ""
+password1 = input("Enter your New password: ") 
+password2 = input("Confirm your New password: ")
+while password1 != password2:
+        
+    print("it doesn't match. Please try again.")
+    password1 = input("Enter your New password: ")
+    password2 = input("Confirm your New password: ")
+if password1 == password2:
+    password = password1
+    print("Your password has been set successfully.")
+    print("")
+    print("========================================================")
+    print("")
+    print("Type 'test' to test your password or 'exit' to quit.")
+    if input() == "test":
+        test_password = ""
+        while test_password != password:
+            test_password = input("Enter your password to test: ")
+            if test_password == password:
+                print("Password is correct!")
+                print("Thank you for using our program. Goodbye!")
+                time.sleep(5)
+            else:
+                print("Incorrect password. Please try again.")
+    else:
+        wait = input("Are you sure you want to exit? (yes/no): ")
+        if wait == "yes":
+            print("Thank you for using our program. Goodbye!")
+            time.sleep(5)
+        else:
+            test_password = ""
+            while test_password != password:
+                test_password = input("Enter your password to test: ")
+                if test_password == password:
+                    print("Password is correct!")
+                    print("Thank you for using our program. Goodbye!")
+                    time.sleep(5)
+                else:
+                    print("Incorrect password. Please try again.")
+        
