a71e2a5cb67f031cfac47bf4fa9e38fc9123b39d
Ali Taheri <ali_taheri1985@hotmail.com>
2026-06-02 13:23:32 +0330

    added a new practice to function-1.py

--- function-1.py
+++ function-1.py
@@ -1,4 +1,4 @@
-#function definition: a function is a reusable block of code that performs a specific task.
+# function definition: a function is a reusable block of code that performs a specific task.
 # It is defined using the def keyword followed by the function name and parentheses.Functions can take parameters,
 # which are values passed to the function when it is called, and they can return a value using the return statement.
 # Functions help in organizing code, improving readability, and promoting code reusability.
@@ -16,13 +16,14 @@ say_hello()
 
 print("=============================================================")
 
-def sum_numbers():
-    a = 5
-    b = 10
-    return a + b # the next line of code will not be executed after the return statement is executed
+first_num = int(input("Enter the first number: "))
+second_num = int(input("Enter the second number: "))
+def sum_numbers(first_num, second_num):
+
+    return first_num + second_num # the next line of code will not be executed after the return statement is executed
     print("This line will not be executed.")
 
-print("The sum of a and b is:", sum_numbers())
+print("The sum of first_num and second_num is:", sum_numbers(first_num, second_num))
 
 print("=============================================================")
 
@@ -37,9 +38,12 @@ print(f"full name is: {full_name(dic1['name'], dic1['family'])}")
 
 print("=============================================================")
 
+
 num = int(input("Enter a number: "))
-def check_even_odd(num):
-    if num % 2 == 0:
-        return "Even"
-    return "Odd"
-print(f"The number {num} is {check_even_odd(num)}.")
+def be_tavan_2(num, power=2):
+    return num ** power
+print(f"this is power 2: {be_tavan_2(num)}")
+power = int(input("Enter the power: "))
+def be_tavan_x(num, power):
+    return num ** power
+print(f"this is power {power}: {be_tavan_x(num,power)}")
